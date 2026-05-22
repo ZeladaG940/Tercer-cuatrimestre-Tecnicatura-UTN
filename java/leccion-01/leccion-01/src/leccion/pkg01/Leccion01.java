eb631d92f4410778104f6b6920a5f11ce35e7d7c
orlando zelada <orlandozeladagira@gmail.com>
2026-05-22 16:49:50 -0300

    se agrego la primera clase-1 de java

---
+++ java/leccion-01/leccion-01/src/leccion/pkg01/Leccion01.java
@@ -0,0 +1,15 @@
+
+package leccion.pkg01;
+
+/**
+ *
+ * @author WinterOS
+ */
+public class Leccion01 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
